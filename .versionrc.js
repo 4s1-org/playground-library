690d55ddaafef286adb1cd692bcaa430685c2c0b
Steffen <steffen@4s1.de>
2021-09-17 11:59:18 +0200

    chore: foo

--- .versionrc.js
+++ .versionrc.js
@@ -1,4 +1,3 @@
 const config = require('@4s1/changelog-config')
 
-console.log(config('https://gitlab.com/4s1/<PROJECT_URL>'))
-module.exports = config('https://gitlab.com/4s1/<PROJECT_URL>')
+module.exports = config('https://gitlab.com/4s1/playground')
