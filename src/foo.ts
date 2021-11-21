ffded73159c1b3f4c6e5c85fbec518674a70d223
Steffen <steffen@4s1.de>
2021-11-21 12:47:19 +0100

    foo

--- src/foo.ts
+++ src/foo.ts
@@ -2,4 +2,9 @@ export class Foo {
   public log() {
     console.log('Foo')
   }
+
+  public login(username: string, password: string) {
+    console.log(username)
+    console.log(password)
+  }
 }
