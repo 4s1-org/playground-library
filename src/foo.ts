bda1a8f4f4de0dbf81e6f6b409eb4fcb5714cac8
Steffen <steffen@4s1.de>
2021-11-21 16:41:08 +0100

    fix: remove code smells

--- src/foo.ts
+++ src/foo.ts
@@ -2,22 +2,4 @@ export class Foo {
   public log() {
     console.log('Foo')
   }
-
-  public login(username: string, password: string): void {
-    console.log(username)
-    console.log(password)
-  }
-
-  public foo(): void {
-    for (let i = 0; i < 10; ) {
-      console.log(i)
-    }
-  }
-
-  public App() {
-    // 'Empty block statement' code smell
-    try {
-    } catch (e) {}
-    return
-  }
 }
