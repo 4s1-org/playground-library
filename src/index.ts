1e1c6df777531e9ffe3a317ba565b029e6b78ec8
Steffen <steffen@4s1.de>
2022-03-01 13:30:57 +0100

    chore: add bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Foo } from './foo'
 
 const password = '12345'
+const url = 'http://not.good.com'
 
 console.log('Hello World')
-console.log('Hello World2', password)
+console.log('Hello World2', password, url)
